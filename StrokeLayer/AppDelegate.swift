b3c4d5bbe818959b71d81ee1647032cdeebd0049
Atsushi Jike <a_jike@yumemi.co.jp>
2018-10-23 10:26:16 +0900

    Initial Commit

---
+++ StrokeLayer/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  StrokeLayer
+//
+//  Created by 寺家 篤史 on 2018/10/23.
+//  Copyright © 2018 Yumemi Inc. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
